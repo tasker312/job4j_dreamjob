30b1b8b13c6c53eaacf818d619317852dd0b92b5
tasker312 <ivan255255@gmail.com>
2025-04-15 13:39:09 +0300

    3. Bootstrap, Table [#504840]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@SuppressWarnings("unused")
 public class IndexController {
     @GetMapping("/index")
     public String getIndex() {
