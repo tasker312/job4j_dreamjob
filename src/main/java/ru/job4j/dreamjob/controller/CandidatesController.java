0ae0371002b43a1db56319d4466bdaaaa03033d1
tasker312 <ivan255255@gmail.com>
2025-04-15 16:22:21 +0300

    9. HTML form. Создание страницы кандидата [#504845]

--- src/main/java/ru/job4j/dreamjob/controller/CandidatesController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidatesController.java
@@ -18,4 +18,9 @@ public class CandidatesController {
         model.addAttribute("candidates", candidateRepository.findAll());
         return "candidates/list";
     }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "candidates/create";
+    }
 }
